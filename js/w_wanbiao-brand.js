d4555ea6fcf1556d7a0a7b8da5c77afa5119b2f1
吴威威 <2727488280@qq.com>
2019-10-15 08:46:00 +0800

    10.16页面修改

--- js/w_wanbiao-brand.js
+++ js/w_wanbiao-brand.js
@@ -1,32 +1,43 @@
 $(function() {
     $(".w_slideshow-a1").eq(0).addClass("w_slideshow-a2").siblings().removeClass("w_slideshow-a2");
-var thimer=setInterval(function() {
     if($(".w_slideshow").hasClass("w_slideshow-top")==false){
-        $(".w_slideshow").addClass("w_slideshow-top");
-        $(".w_slideshow-a1").eq(1).addClass("w_slideshow-a2").siblings().removeClass("w_slideshow-a2");
-    }
-},5000)
-var thimer=setInterval(function() {
-    if($(".w_slideshow").hasClass("w_slideshow-top")==true){
-       $(".w_slideshow").removeClass("w_slideshow-top");
-       $(".w_slideshow-a1").eq(0).addClass("w_slideshow-a2").siblings().removeClass("w_slideshow-a2");
-    }
-    },7000)
-$(".w_blank-top").on("mouseover",function(){
-    clearInterval(thimer);
-})
-$(".w_blank-top").on("mouseout",function(){
-    var thimer=setInterval(function() {
-        if($(".w_slideshow").hasClass("w_slideshow-top")==false){
+        var thimer=setInterval(function() {
             $(".w_slideshow").addClass("w_slideshow-top");
             $(".w_slideshow-a1").eq(1).addClass("w_slideshow-a2").siblings().removeClass("w_slideshow-a2");
-        }
-    },5000)
-    var thimer=setInterval(function() {
-        if($(".w_slideshow").hasClass("w_slideshow-top")==true){
-           $(".w_slideshow").removeClass("w_slideshow-top");
-           $(".w_slideshow-a1").eq(0).addClass("w_slideshow-a2").siblings().removeClass("w_slideshow-a2");
-        }
-    },7000)
+        },5000)
+    }else {
+        var thimer1=setInterval(function() {
+            
+            $(".w_slideshow").removeClass("w_slideshow-top");
+            $(".w_slideshow-a1").eq(0).addClass("w_slideshow-a2").siblings().removeClass("w_slideshow-a2");
+        },10000)
+    }
 })
+$(function(){
+     var a=0;
+    $(".w_brand-lt4").on("click",function(){
+        a++
+        if(a<15){
+            $(".w_brand-lt").animate({
+                "left":a*-150+"px"
+            },1000)
+        }else{
+            a=0;
+            $(".w_brand-lt").css("left",a*-150+"px")
+        }
+    })
+    $(".w_brand-lt1").on("click",function(){
+        a--
+        if(a>0){
+            $(".w_brand-lt").animate({
+                "left":a*-150+"px"
+            },1000)
+        }else{
+            a=0;
+            $(".w_brand-lt").animate({
+                "left":a*-150+"px"
+            },1000)
+        }
+        
+    })
 })
