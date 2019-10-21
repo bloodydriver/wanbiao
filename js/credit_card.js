5a6548fc50466b624906e0be22b67ec8f8fd6915
夏宏伟 <873735398@qq.com>
2019-10-21 20:29:37 +0800

    帮助中心页面和信用卡中心页面 信用卡页面只完成了一部分

--- js/credit_card.js
+++ js/credit_card.js
@@ -1,4 +1,6 @@
 $(function() {
+
+    // 信用卡中心页面
     //顶部 手机万表 鼠标经过
     $(".x_hover_phonewb").mouseenter(function() {
         $(".x_hover_phonewb+div").slideToggle("fast");
@@ -19,5 +21,23 @@ $(function() {
     
     $(".x_top_server").mouseleave(function() {
         $(this).slideUp("fast");
+    }) 
+    // 轮播
+    new Swiper('.swiper-container', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        loop: true,
+    });
+
+    // help页面
+    // 左侧导航栏鼠标经过
+    $(".index_left_nav").css("display","none")
+    $(".nav_left").mouseenter(function() {
+        $(".index_left_nav").slideDown("fast");
+    })
+    $(".nav_left").mouseleave(function() {
+        $(".index_left_nav").slideUp();
     })
 })
