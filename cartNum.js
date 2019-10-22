8020638eadca1a59de2adc1464c320539d420b1f
刘振军 <1580235885@qq.com>
2019-10-22 20:35:41 +0800

    已完善

--- cartNum.js
+++ cartNum.js
@@ -1,8 +1,7 @@
 $(function() {
-    //存储购物车页面代码及购物车数量
+    //存储购物车页面代码及购物车数量(公用)
     var cartNum=localStorage.getItem("cartNum")>0?localStorage.getItem("cartNum"):0;
     var cartCode=localStorage.getItem("cartCode")!=null?localStorage.getItem("cartCode"):"";
-    // console.log(cartCode)
     $(".cartNum span").text("("+cartNum+")")
     if(cartNum>0) {
         $(".cartNum").css("color","#cc5252")
@@ -14,33 +13,29 @@ $(function() {
         cartNum++;
         localStorage.setItem("cartNum",cartNum)
         $(this).parent().parent().parent().addClass("addCart");
-        cartCode="";
-        for(var i=0;i<$(".addCart").length;i++) {
-            // console.log($(".addCart .w_cont-bt7").eq(i).text().substr(1).split(",").join(""))
             cartCode+=`
             <li>
                 <i class="check"></i>
                 <p>
-                    <img src="${$(".addCart .w_cont_img1").eq(i)[0].src}" alt="">
+                    <img src="${$(".addCart .w_cont_img1").eq($(".addCart").index($(this).parent().parent().parent()))[0].src}" alt="">
                 </p>
                 <div class="content">
-                    <p>${$(".addCart .w_cont-bt8").eq(i).text()}</p>
+                    <p>${$(".addCart .w_cont-bt8").eq($(".addCart").index($(this).parent().parent().parent())).text()}</p>
                     <p>真皮 黑色</p>
                 </div>
                 <div class="control">
-                    <span>￥<span class="price">${$(".addCart .w_cont-bt7").eq(i).text().split(" ").join("").substr(1).split(",").join("")}</span></span>
+                    <span>￥<span class="price">${$(".addCart .w_cont-bt7").eq($(".addCart").index($(this).parent().parent().parent())).text().split(" ").join("").substr(1).split(",").join("")}</span></span>
                     <div>
                         <button class="sub" type="button">-</button>
                         <input class="number" type="text" value="1" disabled>
                         <button class="add" type="button">+</button>
                     </div>
-                    <span class="priceAll" style="font-weight: 700;">￥${$(".addCart .w_cont-bt7").eq(i).text().split(" ").join("").substr(1).split(",").join("")}</span>
+                    <span class="priceAll" style="font-weight: 700;">￥${$(".addCart .w_cont-bt7").eq($(".addCart").index($(this).parent().parent().parent())).text().split(" ").join("").substr(1).split(",").join("")}</span>
                     <a class="del" href="javascript:void(0)">删除</a>
                 </div>
                 <a href="javascript:void(0)">移入收藏</a>
             </li>
             `
-        }
         localStorage.setItem("cartCode",cartCode)
         if(cartNum>0) {
             $(".cartNum").css("color","#cc5252")
